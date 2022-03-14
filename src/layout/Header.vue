c074bbed95c0ba9ef932eb6c1adcbf4970e232d5
sing-visionpro1 <sing.eaglevision@gmail.com>
2022-03-14 13:51:21 -0700

    story

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,6 +1,70 @@
 <template>
-  <div class="">
-    <div class="bg-black">
+  <div class="bg-black">
+    <div class="hide">
+      <nav
+        class="
+         
+          md:flex md:justify-center md:items-center
+          
+        "
+      >
+        
+
+
+        <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+       
+          <div class="flex justify-start">
+            <img src="./../assets/images/babes.png" class=""/>
+          </div>  
+      
+            <div class="w-6/12 flex justify-center space-x-4">
+              <ul 
+                :class="showMenu ? 'flex' : 'hidden' "
+                class="
+                  
+                  flex-col
+                  mt-8
+                  space-y-4
+                  md:flex
+                  md:space-y-0
+                  md:flex-row
+                  md:items-center
+                  md:space-x-2
+                  md:mt-0
+                  justify-center
+                "
+              >
+                <li class="text-xs font-bold text-white hover:text-pink-400">
+                  STORY
+                </li>
+                <li class="text-xs font-bold text-white hover:text-pink-400">
+                  CLUB
+                </li>
+                <li class="text-xs font-bold text-white hover:text-pink-400">
+                  CHARITYDONATION
+                </li>
+                <li class="text-xs font-bold text-white hover:text-pink-400">
+                  BOOBMAP
+                </li>
+                <li class="text-xs font-bold text-white hover:text-pink-400">
+                  FAQ
+                </li>
+                <li class="text-xs font-bold text-white hover:text-pink-400">
+                  CONTACT
+                </li>
+              </ul>
+            </div>
+        <div class="hide">
+        <div class=" flex space-x-4 ">
+          <img src="./../assets/images/connect_btn.png">
+        </div>
+        </div>
+        <div class="w-1/12"></div>
+      </nav>
+    </div>
+    
+
+    <div class="header_hide">
       <nav
         class="
          
@@ -44,11 +108,7 @@
               style="font-size:90px; font-family:Bodoni Bd BT">babes</p>
       
         </div>
-        <div class="hide">
-          <div class="flex justify-start">
-            <img src="./../assets/images/babes.png" class=""/>
-          </div>  
-        </div>
+        
             <div class="w-6/12 flex justify-center space-x-4">
               <ul 
                 :class="showMenu ? 'flex' : 'hidden' "
@@ -86,11 +146,7 @@
                 </li>
               </ul>
             </div>
-        <div class="hide">
-        <div class=" flex space-x-4 ">
-          <img src="./../assets/images/connect_btn.png">
-        </div>
-        </div>
+        
         <div class="w-1/12"></div>
       </nav>
     </div>
